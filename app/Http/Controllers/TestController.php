ef25c05d41cb97b243681123a40ce3b8ed717de6
thomas <532747343@qq.com>
2022-11-23 13:30:36 +0800

    laravel_shop

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class TestController extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function test()
+    {
+        return test_helper();
+    }
+}
