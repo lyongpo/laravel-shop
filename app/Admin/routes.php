cb66eb40929b226b26ce15b8fc6db2bab92f6af2
thomas <532747343@qq.com>
2022-11-26 11:48:58 +0800

    管理后台 - 商品列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,4 +12,5 @@ Route::group([
 
     $router->get('/', 'HomeController@index')->name('admin.home');
     $router->get('users', 'UserController@index');
+    $router->get('products', 'ProductsController@index');
 });
