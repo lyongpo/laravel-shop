3c662282ea25910f97a2a3a554349d16306db419
thomas <532747343@qq.com>
2022-11-27 11:57:57 +0800

    异常处理

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -13,9 +13,8 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        InvalidRequestException::class,
     ];
-
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
