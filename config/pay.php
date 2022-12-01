0676325f5156e92b9320873181263308b0418107
thomas <532747343@qq.com>
2022-12-01 17:35:01 +0800

    集成支付宝

---
+++ config/pay.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'alipay' => [
+        'app_id'         => '',
+        'ali_public_key' => '',
+        'private_key'    => '',
+        'log'            => [
+            'file' => storage_path('logs/alipay.log'),
+        ],
+    ],
+
+    'wechat' => [
+        'app_id'      => '',
+        'mch_id'      => '',
+        'key'         => '',
+        'cert_client' => '',
+        'cert_key'    => '',
+        'log'         => [
+            'file' => storage_path('logs/wechat_pay.log'),
+        ],
+    ],
+    'app_id' => '你在支付宝沙箱看到的appid',
+    'ali_public_key' => '支付宝沙箱显示的公钥',
+    'private_key' => '刚刚生成的私钥',
+];
