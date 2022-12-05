47436dea4b8c0abfdf4d56739d579edc85f3317a
thomas <532747343@qq.com>
2022-12-05 12:50:49 +0800

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -49,8 +49,9 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
     Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
     Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
-
     Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
+
+    Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 
 });
 Route::redirect('/', '/products')->name('root');
